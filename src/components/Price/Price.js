1120b4df523cc4395f5a2bfcbfd343c3eff9cc4a
Devin Raleigh <devinkr@gmail.com>
2022-06-09 15:36:12 -0600

    Add Price

--- src/components/Price/Price.js
+++ src/components/Price/Price.js
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
 import './Price.css';
+import { useParams } from 'react-router-dom';
 
 const coindeskURL = 'https://api.coindesk.com/v1/bpi/currentprice/';
 
 const Price = ({ price, setPrice }) => {
-	const currency = '';
+	const { currency } = useParams();
+
 	useEffect(() => {
 		const url = `${coindeskURL}${currency}.json`;
 
@@ -17,12 +19,17 @@ const Price = ({ price, setPrice }) => {
 			.catch((err) => {
 				console.error(err);
 			});
+
+		return () => {
+			// Cleanup when the component unmounts so you don't see old price while new one is getting fetched.
+			setPrice(null);
+		};
 	}, []);
 
 	return (
 		<div>
 			<h1>Bitcoin price in {currency}</h1>
-			<div className='price'>{price}</div>
+			<div className='price'>{price ? price : 'Data not found'}</div>
 		</div>
 	);
 };
