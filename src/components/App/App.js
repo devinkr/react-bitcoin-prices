3aacf9e6fac7c1114f8054d1db49e213a7caaae2
Devin Raleigh <devinkr@gmail.com>
2022-06-09 12:56:50 -0600

    Add react-router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,21 @@
-import { useState } from "react";
-import Home from "../Home/Home";
-import "./App.css";
+import { useState } from 'react';
+import Home from '../Home/Home';
+import './App.css';
 
-function App(){
-  const [price, setPrice] = useState()
+function App() {
+	const [price, setPrice] = useState();
 
-    return (
-      <div>
-        <nav>
-          <img
-            src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png"
-            alt=""
-          />
-          <h1>Bitcoin prices</h1>
-        </nav>
-        <main>
-          <Home />
-        </main>
-      </div>
-    );
+	return (
+		<div>
+			<nav>
+				<img src='https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png' alt='' />
+				<h1>Bitcoin prices</h1>
+			</nav>
+			<main>
+				<Home />
+			</main>
+		</div>
+	);
 }
 
 export default App;
