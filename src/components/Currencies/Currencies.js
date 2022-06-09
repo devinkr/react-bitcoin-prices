0f22423f683e76b70ee2bc5b71a9ae4e1632295c
Devin Raleigh <devinkr@gmail.com>
2022-06-09 15:35:55 -0600

    Add Currencies

--- src/components/Currencies/Currencies.js
+++ src/components/Currencies/Currencies.js
@@ -1,18 +1,19 @@
-import "./Currencies.css";
-import listOfCurrencies from "./list.json";
+import './Currencies.css';
+import listOfCurrencies from './list.json';
+import { Link } from 'react-router-dom';
 
 const Currencies = () => {
-    let list = listOfCurrencies.map(item => {
-      return (
-        <div className="currency" key={item.currency}>
-          <p>
-            <a href={"/price/" + item.currency}>{item.currency}</a>:{" "}
-            {item.country}
-          </p>
-        </div>
-      );
-    });
-    return <div>{list}</div>;
-}
+	const list = listOfCurrencies.map((item) => {
+		return (
+			<div className='currency' key={item.currency}>
+				<p>
+					<Link to={'/price/' + item.currency}>{item.currency}</Link>:{' '}
+					{item.country}
+				</p>
+			</div>
+		);
+	});
+	return <div>{list}</div>;
+};
 
 export default Currencies;
